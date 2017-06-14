6db2c258d7652d07baa5f131d3a793cce94edbaa
IvanaVranic <ivana.z.petrovic@gmail.com>
2017-06-14 11:12:52 +0200

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,22 @@
+#load libraries
+library(dplyr)
+library(lubridate)
+#read the file
+house<-read.csv("household_power_consumption.txt", sep = ";")
+#make table data frame
+tabhouse<-tbl_df(house)
+#filter by two required days
+twodays<-filter(tabhouse, tabhouse$Date=="1/2/2007"|tabhouse$Date=="2/2/2007")
+#add new column "DateTime" which is Date and Time columns joined together and save in new table "twodaysmut"
+twodaysmut<-mutate(twodays, DateTime=paste(twodays$Date, twodays$Time, sep=" "))
+#make DateTimeClass column which is of "POSIXct" "POSIXt" class
+twodaysmut<-mutate(twodaysmut, DateTimeClass=dmy_hms(twodaysmut$DateTime))
+#create .png file in working directory
+png("plot3.png", width = 480, height = 480)
+#plot the graph
+plot(twodaysmut$DateTimeClass, as.numeric(as.character(twodaysmut$Sub_metering_1)), type = "l", xlab = "", ylab = "Energy sub metering")
+lines(twodaysmut$DateTimeClass, as.numeric(as.character(twodaysmut$Sub_metering_2)), col="red")
+lines(twodaysmut$DateTimeClass, as.numeric(as.character(twodaysmut$Sub_metering_3)), col="blue")
+legend(x="topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty=c(1,1,1))
+#close the device
+dev.off()
